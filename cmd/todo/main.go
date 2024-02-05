2022340bf5ece35153cb4b1bbb63090315f61308
pokadev <men101@protonmail.com>
2024-02-05 19:15:14 +0300

    add in main.go flag -add

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
 
-func main(){
-  fmt.Println("Todo")
+	todo "github.com/pokadev/todo_golang_cli"
+)
+
+const (
+	todoFile = "todo.json"
+)
+
+func main() {
+	add := flag.Bool("add", false, "add a new todo")
+	flag.Parse()
+	todos := &todo.Todos{}
+
+	if err := todos.Load(todoFile); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+
+	switch {
+	case *add:
+		todos.Add("Sample todo")
+    err := todos.Store(todoFile)
+    if err != nil {
+      fmt.Fprintln(os.Stderr, err.Error())
+      os.Exit(1)
+    }
+	default:
+		fmt.Fprintln(os.Stdout, "invalid command")
+		os.Exit(0)
+	}
 }
