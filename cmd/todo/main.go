77687c7e4e3de0d45ad90ee0e701b27fbb554b02
pokadev <men101@protonmail.com>
2024-02-05 21:34:14 +0300

    add in main.go func getInput

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
+	"io"
 	"os"
+	"strings"
 
 	todo "github.com/pokadev/todo_golang_cli"
 )
@@ -64,4 +67,24 @@ func main() {
 		fmt.Fprintln(os.Stdout, "invalid command")
 		os.Exit(0)
 	}
+}
+
+func getInput(r io.Reader, args ...string) (string, error) {
+
+	if len(args) > 0 {
+		return strings.Join(args, " "), nil
+	}
+
+	scanner := bufio.NewScanner(r)
+	scanner.Scan()
+	if err := scanner.Err(); err != nil {
+		return "", err
+	}
+
+	text := scanner.Text()
+
+	if len(text) == 0 {
+		return "", fmt.Errorf("empty todo is not allowed")
+	}
+	return text, nil
 }
