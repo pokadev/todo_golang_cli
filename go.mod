0cf482301815df0ab6e0cf0d7f2cfb656a1180ce
pokadev <men101@protonmail.com>
2024-02-05 21:57:13 +0300

    styling table pt.1

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/pokadev/todo_golang_cli
 
 go 1.21.6
+
+require (
+	github.com/alexeyco/simpletable v1.0.0 // indirect
+	github.com/mattn/go-runewidth v0.0.15 // indirect
+	github.com/rivo/uniseg v0.4.6 // indirect
+)
--- todo.go
+++ todo.go
@@ -6,6 +6,8 @@ import (
 	"fmt"
 	"os"
 	"time"
+
+	"github.com/alexeyco/simpletable"
 )
 
 type item struct {
@@ -75,7 +77,58 @@ func (t *Todos) Store(filename string) error {
 }
 
 func (t *Todos) Print() {
-	for i, item := range *t {
-		fmt.Printf("%d - %s\n", i+1, item.Task)
+
+	table := simpletable.New()
+
+	table.Header = &simpletable.Header{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Text: "#"},
+			{Align: simpletable.AlignCenter, Text: "Task"},
+			{Align: simpletable.AlignCenter, Text: "Done"},
+			{Align: simpletable.AlignCenter, Text: "Created At"},
+			{Align: simpletable.AlignCenter, Text: "Completed At"},
+		},
 	}
+
+	var cells [][]*simpletable.Cell
+
+	for idx, item := range *t {
+    task := blue(item.Task)
+    if item.Done {
+      task = green(fmt.Sprintf("\u2705 %s", item.Task))
+    }
+		cells = append(cells, *&[]*simpletable.Cell{
+			{Text: fmt.Sprintf("%d", idx+1)},
+			{Text: task},
+			{Text: fmt.Sprintf("%t", item.Done)},
+			{Text: item.CreatedAt.Format(time.RFC822)},
+			{Text: item.CompletedAt.Format(time.RFC822)},
+		})
+	}
+
+	table.Body = &simpletable.Body{
+		Cells: cells,
+	}
+
+	table.Footer = &simpletable.Footer{
+		Cells: []*simpletable.Cell{
+			{Align: simpletable.AlignCenter, Span: 5, Text: red(fmt.Sprintf("You have %d pending todos ", t.CountPending()))},
+		},
+	}
+
+	table.SetStyle(simpletable.StyleUnicode)
+
+	table.Println()
+}
+
+
+func (t *Todos) CountPending() int {
+  total := 0
+
+  for _, item := range *t {
+    if !item.Done {
+      total++
+    }
+  }
+  return total
 }
